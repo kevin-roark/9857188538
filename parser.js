02ee8ba413a8b8c57af27dd748268b348c0fba75
Kevin Roark <ker2143@columbia.edu>
2014-11-11 16:06:15 -0500

    Create parser.js

---
+++ parser.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+
+if (process.argv.length < 1) {
+  console.log('need something 2 parse');
+  return;
+}
+
+var file = process.argv[0];
+var lines = fs.readFileSync(file).toString().split('\n');
+
+var parsed = [];
+
+lines.forEach(function(line) {
+  var segments = line.split(' ||| ');
+  if (segments.length != 3) return;
+  
+  var data = {
+    line: segments[0],
+    duration: parseFloat(segments[1]),
+    amplitude: parseFloat(segments[2])
+  };
+  
+  parsed.push(data);
+});
+
+var outfile = (process.argv.length >= 2)? process.argv[1] : 'poem.js';
+fs.writeFileSync(outfile, JSON.stringify(parsed));

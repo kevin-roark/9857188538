5f6c965bdc27bea7265c5214ecc01717acad5304
Kevin Roark <ker2143@columbia.edu>
2014-11-11 23:17:20 -0500

    mmm aggro

--- boiler/main.js
+++ boiler/main.js
@@ -1,6 +1,9 @@
 
 var MIN_FONT = 12;
 var MAX_FONT = 72;
+var MAX_AGG = 5;
+var MIN_AGG = 0;
+var FONT_AGG_MULT = 2;
 
 var activeBodyClass = '';
 
@@ -9,14 +12,15 @@ window.poem.forEach(function(lineData) {
 });
 
 function handleLineData(lineData) {
-  lineData.fontSize = ((MAX_FONT - MIN_FONT) * lineData.amplitude) + 'px';
-  lineData.left = ($(window).width() * 0.6 * Math.random()) + 'px';
-  lineData.top = ($(window).height() * 0.95 * Math.random()) + 'px';
+  lineData.fontSize = ((MAX_FONT - MIN_FONT) * lineData.amplitude);
+  lineData.aggressiveness = (lineData.amplitude > 0.5)? (MAX_AGG - MIN_AGG) * 2 * (lineData.amplitude - 0.5) : 0;
+  lineData.left = ($(window).width() * 0.64 * Math.random());
+  lineData.top = ($(window).height() * 0.88 * Math.random());
+
+  console.log(lineData);
   
   var line = $('<div class="poem">' + lineData.line + '</div>');
-  line.css('font-size', lineData.fontSize);
-  line.css('top', lineData.top);
-  line.css('left', lineData.left);
+  updateCssForLine(line, lineData);
   line.css('display', 'none');
   $('body').append(line);
   
@@ -30,12 +34,39 @@ function handleLineData(lineData) {
       var char = lineData.line.charAt(i);
       updateScreenForCharacter(char, timePerCharacter * i);
     }
+
+    var aggInterval = setInterval(function() {
+      lineData.top += Math.floor((Math.random() - 0.5) * 2 * lineData.aggressiveness);
+      lineData.left += Math.floor((Math.random() - 0.5) * 2 * lineData.aggressiveness);
+      lineData.fontSize += (Math.random() - 0.5) * FONT_AGG_MULT * lineData.aggressiveness;
+      
+      sanitizeLineData(lineData);
+      updateCssForLine(line, lineData);
+    }, 30);
     
     // go away
     setTimeout(function() {
+      clearInterval(aggInterval);
       line.fadeOut(200);
     }, lineData.duration * 1000);
   }, lineData.onset * 1000);
+}
+
+function updateCssForLine(line, lineData) {
+  line.css('font-size', lineData.fontSize + 'px');
+  line.css('top', lineData.top + 'px');
+  line.css('left', lineData.left + 'px');
+}
+
+function sanitizeLineData(lineData) {
+  if (lineData.fontSize < 6) lineData.fontSize = 6;
+  if (lineData.fontSize > 200) lineData.fontSize = 200;
+
+  if (lineData.left < 0) lineData.left = 0;
+  if (lineData.left > $(window).width()) lineData.left = $(window).width();
+
+  if (lineData.top < 0) lineData.top = 0;
+  if (lineData.top > $(window).height()) lineData.top = $(window).height();
 }
 
 function updateScreenForCharacter(char, delay) {
